905dddd22e2e96dd1937f86edceff14c733576a6
mvolarik <marianitovlk@gmail.com>
2015-01-24 18:52:48 -0300

    Orden y limpieza del código
    + Se borra bloques de código comentados que no utilzaremos.
    + Se acomodan los imports.

--- trunk/cefyt/sia/views.py
+++ trunk/cefyt/sia/views.py
@@ -1,37 +1,21 @@
-from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from sia.models import Pais, Alumno, Cursado
-from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+
 from django.contrib.auth import authenticate, login
-from sia.forms import UsuarioForm, RegistroForm
+from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import redirect
+
+from sia.models import Pais, Alumno, Cursado
+from sia.forms import UsuarioForm, RegistroForm
 
 
 @login_required
 def index(request):
 
-    context = {
-               'mensaje': 'hola_mundo'}
+    context = {'mensaje': 'hola_mundo'
+              }
     return render(request, 'sia/index.html', context)
 
-# def login(request):
-#   form = myform
-
-#   if request.method == 'GET':
-#     pass
-
-#   elif request.methon == 'POST':
-#     if form.is_valid():
-#       usuario = request.POST.get('usuario')
-#       contrasenia = request.POST.get('password')
-#       usuario = authenticate(usuario, contrasenia)
-#       if usuario.is_authenticated:
-#         redirect()
-  
-
-#   context = {'form' = form}
-#   return render(request, 'mytemplate.html', context)
 
 @login_required
 def cuenta(request):
@@ -41,10 +25,10 @@ def cuenta(request):
               }
 
     if request.method == "POST":
-      inscripcion = Cursado.objects.get(id=request.POST.get('curso'))
-      #usuario = User.objects.get(username=request.user.username)
-      alumno = Alumno.objects.get(usuario__username='marianitovlk@gmail.com')
-      inscripcion.alumno.add(alumno)
+        inscripcion = Cursado.objects.get(id=request.POST.get('curso'))
+        #usuario = User.objects.get(username=request.user.username)
+        alumno = Alumno.objects.get(usuario__username='marianitovlk@gmail.com')
+        inscripcion.alumno.add(alumno)
 
     return render(request, 'sia/cuenta.html', context)
 
@@ -68,8 +52,6 @@ def registro(request):
             #(object, created)
             if not creado:
                 usuario_existente = True
-                pass
-                #exploted
             else:
                 usuario.set_password(request.POST.get('password'))
                 usuario.save()
@@ -95,63 +77,3 @@ def registro(request):
               }
 
     return render(request, 'sia/registro.html', context)
-
-#def cursos_dusponible(request):
-#    activos = Cursado.objects.all().filter(inscripcion_abierta = True)
-  
-
-
-
-
-#def crear_registro(request):
-#    apellido = request.POST.get['apellido']
-#    nombres = request.POST['nombres']
-#    documento = request.POST['documento']
-#    fecha_de_nacimiento = request.POST['fecha_de_nacimiento']
-#    pais = request.POST['pais']
-#    provincia = request.POST['provincia']
-#    localidad = request.POST['localidad']
-#    domicilio = request.POST['domicilio']
-#    telefono = request.POST['telefono']
-#    telefono_alter = request.POST['telefono_alter']
-#    email = request.POST['email']
-#    pwd = request.POST['pwd']
-#    pais = Pais.objects.get(id=pais)
-#
-#    if not Alumno.objects.filter(email="email"):
-#      #definir web de error
-#      return HttpResponseRedirect('/sia')
-#    else:
-#      nuevo_alumno = Alumno(apellido = apellido,
-#                          nombres = nombres,
-#                          documento = documento,
-#                          fecha_de_nacimiento = fecha_de_nacimiento,
-#                          pais = pais,
-#                          provincia = provincia,
-#                          localidad = localidad,
-#                          domicilio = domicilio,
-#                          telefono = telefono,
-#                          telefono_alter = telefono_alter,
-#                          email = email)
-#
-#      
-#    
-#
-#      nuevo_alumno.save()
-#      
-#      # TO DO:
-#      # Catchear excepciones
-#      user = User.objects.create_user(email, email, pwd)
-#
-#
-#      datos = apellido + nombres + documento + fecha_de_nacimiento + provincia + localidad + domicilio + telefono + telefono_alter + email + pwd
-#      
-#
-#
-#      # TO DO: Crear un nuevo usuario usando el sistema de usuario de Django:
-#      # Ver: https://docs.djangoproject.com/en/1.6/topics/auth/default/#user-objects
-#
-#      # TO DO: Tener en cuenta, agregar relaciones ManyToMany
-#      # Cursado.objects.all()[0].alumno.add('diego@guzman.com')
-#
-#      return HttpResponseRedirect('/sia')
