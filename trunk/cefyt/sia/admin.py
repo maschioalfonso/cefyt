2a36654634ce1e252e14431398ab7502fc2d9757
mvolarik <marianitovlk@gmail.com>
2015-03-11 16:45:56 -0300

    Cambios en sia/admin y models.py
    + Se agrega 'unique' a los campos 'nombre' de los modelos.
    
    + Se agrega el campo de descripción a Cuota.
    
    + Columnas de información en sia/admin en:
    - Alumno
    - Curso
    - Cursado
    - DescubrimientoCurso

--- trunk/cefyt/sia/admin.py
+++ trunk/cefyt/sia/admin.py
@@ -1,15 +1,28 @@
 from django.contrib import admin
 from sia.models import Alumno, Pais, Materia, Curso, Cursado, Cuota, DescubrimientoOpcion, DescubrimientoCurso
 
+
+class AlumnoAdmin(admin.ModelAdmin):
+    list_display = ('usuario', 'documento', 'fecha_de_nacimiento', 'pais', 'provincia', 'localidad', 'domicilio', 'telefono', 'telefono_alter')
+
+class CursoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'descripcion')
+
+class CursadoAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'curso', 'duracion', 'costo_inscripcion_pesos', 'costo_inscripcion_dolares', 'costo_certificado_pesos', 'costo_certificado_dolares', 'valor_cuota_pesos', 'valor_cuota_dolares', 'inscripcion_abierta')
+
 class CuotaAdmin(admin.ModelAdmin):
     list_display = ('cursado', 'alumno', 'numero', 'valor_cuota_pesos', 'valor_cuota_dolares', 'costo_certificado_dolares', 'costo_certificado_pesos', 'fecha_de_pago', 'comprobante', 'pagado')
 
+class DescubrimientoCursoAdmin(admin.ModelAdmin):
+    list_display = ('cursada', 'alumno', 'opcion')
+
+
 admin.site.register(Pais)
-admin.site.register(Alumno)
+admin.site.register(Alumno, AlumnoAdmin)
 admin.site.register(Materia)
-admin.site.register(Curso)
-admin.site.register(Cursado)
+admin.site.register(Curso, CursoAdmin)
+admin.site.register(Cursado, CursadoAdmin)
 admin.site.register(Cuota, CuotaAdmin)
 admin.site.register(DescubrimientoOpcion)
-admin.site.register(DescubrimientoCurso)
-
+admin.site.register(DescubrimientoCurso, DescubrimientoCursoAdmin)
--- trunk/cefyt/sia/models.py
+++ trunk/cefyt/sia/models.py
@@ -4,19 +4,24 @@ from django.contrib.auth.models import User
 from django.core.validators import MaxValueValidator, MinValueValidator
 MAX_LENGTH = 255
 
+
 class Pais(models.Model):
+
     class Meta:
         verbose_name_plural = "Paises"
-    nombre = models.CharField(max_length=MAX_LENGTH)
+
+    nombre = models.CharField(unique=True, max_length=MAX_LENGTH)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.nombre
 
 
 class Alumno(models.Model):
+
     class Meta:
         verbose_name_plural = "Alumnos"
-    usuario = models.OneToOneField(User)  
+
+    usuario = models.OneToOneField(User)
     documento = models.CharField(max_length=MAX_LENGTH)
     fecha_de_nacimiento = models.DateField()
     pais = models.ForeignKey("Pais")
@@ -25,15 +30,17 @@ class Alumno(models.Model):
     domicilio = models.CharField(max_length=MAX_LENGTH)
     telefono = models.CharField(max_length=MAX_LENGTH)
     telefono_alter = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
-    
+
     def __str__(self):              # __unicode__ on Python 2
         return self.usuario.username
 
 
 class Materia(models.Model):
+
     class Meta:
         verbose_name_plural = "Materias"
-    nombre = models.CharField(max_length=MAX_LENGTH)
+
+    nombre = models.CharField(unique=True, max_length=MAX_LENGTH)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.nombre
@@ -44,7 +51,7 @@ class Curso(models.Model):
     class Meta:
         verbose_name_plural = "Cursos"
 
-    nombre = models.CharField(max_length=MAX_LENGTH)
+    nombre = models.CharField(unique=True, max_length=MAX_LENGTH)
     materias = models.ManyToManyField(Materia)
     descripcion = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
 
@@ -57,7 +64,7 @@ class Cursado(models.Model):
     class Meta:
         verbose_name_plural = "Cursados"
 
-    nombre = models.CharField(max_length=MAX_LENGTH, unique=True)
+    nombre = models.CharField(unique=True, max_length=MAX_LENGTH)
     curso = models.ForeignKey(Curso)
     alumno = models.ManyToManyField(Alumno, blank=True, null=True)
     duracion = models.IntegerField(default=0,validators=[MinValueValidator(0),MaxValueValidator(12)]) #Restringir de 1 a 12. CHOICES de 1 a 12 !
@@ -78,13 +85,15 @@ class DescubrimientoOpcion(models.Model):
 
     class Meta:
         verbose_name_plural = "Descubrimiento opciones"
-    opcion = models.CharField(max_length=MAX_LENGTH)
+
+    opcion = models.CharField(unique=True, max_length=MAX_LENGTH)
 
     def __str__(self):              # __unicode__ on Python 2
         return self.opcion
 
 
-class DescubrimientoCurso(models.Model): 
+class DescubrimientoCurso(models.Model):
+
     cursada = models.ForeignKey(Cursado)
     alumno = models.ForeignKey(Alumno)
     opcion = models.ForeignKey(DescubrimientoOpcion)
@@ -92,18 +101,21 @@ class DescubrimientoCurso(models.Model):
     def __str__(self):              # __unicode__ on Python 2
         return self.alumno.usuario.username + ' - ' + self.cursada.nombre + '-> ' + self.opcion.opcion
 
+
 class Cuota(models.Model):
+
     alumno = models.ForeignKey(Alumno)
     cursado = models.ForeignKey(Cursado)
     numero = models.IntegerField(default=0)
     fecha_de_pago = models.DateField(blank=True, null=True)
     comprobante = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
+    descripcion = models.CharField(max_length=MAX_LENGTH, blank=True, null=True)
     pagado = models.BooleanField(default=False)
     costo_certificado_dolares = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
     costo_certificado_pesos = models.DecimalField( decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
     valor_cuota_pesos = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
     valor_cuota_dolares = models.DecimalField(decimal_places=2, max_digits=12, validators=[MinValueValidator(0)])
-    
+
     def __str__(self):              # __unicode__ on Python 2
         etiqueta = self.alumno.usuario.first_name + ': ' + self.cursado.nombre + ' - ' + 'Cuota: ' + str(self.numero)
         return etiqueta
