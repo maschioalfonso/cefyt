86aeef4ac86cd62d4e255fdb7d7527d59dfbb2f7
mvolarik <marianitovlk@gmail.com>
2015-08-14 10:17:26 -0300

    # -*- encoding: utf-8 -*- para admin.py

--- trunk/cefyt/sia/admin.py
+++ trunk/cefyt/sia/admin.py
@@ -1,3 +1,4 @@
+# -*- encoding: utf-8 -*-
 from django.contrib import admin
 from sia.models import (Alumno, Pais, Materia, Curso, Cursado, Cuota,
                         DescubrimientoOpcion, DescubrimientoCurso)
