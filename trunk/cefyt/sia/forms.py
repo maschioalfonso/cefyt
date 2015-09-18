1144167648b7010932ad4da33dc9f530beef2326
alfons <maschioalfonso@gmail.com>
2015-09-18 10:12:19 -0400

    Form update

--- trunk/cefyt/sia/forms.py
+++ trunk/cefyt/sia/forms.py
@@ -1,8 +1,14 @@
 # -*- coding: utf-8 -*-
 from django.forms import (ModelForm, PasswordInput, EmailField, CharField,
-                          ModelChoiceField, IntegerField)
+                          ModelChoiceField, IntegerField, DateField)
 from sia.models import Alumno, Pais
+from django.db import models
+from django.forms.extras.widgets import SelectDateWidget
+from django.contrib.admin.widgets import AdminDateWidget
+from django.contrib.admin import widgets
 
+
+YEARS_CHOISES = tuple([str(year) for year in range(1920, 2015)])
 
 class RegistroForm(ModelForm):
     nombre = CharField()
@@ -12,6 +18,7 @@ class RegistroForm(ModelForm):
     password = CharField(widget=PasswordInput(), label="Contraseña")
     pais = ModelChoiceField(queryset=Pais.objects.all(),
                             empty_label=None, label='País')
+    fecha_de_nacimiento = DateField(widget=SelectDateWidget(years=YEARS_CHOISES))
 
     class Meta:
         model = Alumno
